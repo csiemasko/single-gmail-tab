6df05b6146a1f21f2e8974e6385efb2401974594
Christian Siemasko <c.siemasko@gmail.com>
2020-04-03 16:59:01 -0400

    Initial commit

---
+++ single-tab.js
@@ -0,0 +1,34 @@
+var observeObject = (() => {
+    let _obs = window.MutationObserver || window.WebKitMutationObserver
+    return (o, cb) => {
+        if (!o || !o.nodeType === 1) return
+        if (_obs) {
+            let _observer = new MutationObserver((mutations, observer) => {
+                cb(mutations)
+            })
+            _observer.observe(o, {
+                childList: true,
+                subtree: true
+            })
+        } else if (window.addEventListener) {
+            o.addEventListener('DOMNodeInserted', cb, false)
+            o.addEventListener('DOMNodeRemoved', cb, false)
+        }
+    }
+})()
+setTimeout(() => {
+    let rt = document.querySelector('div.gb_cg')
+    rt.childNodes.forEach(a => a.removeAttribute('target'))
+    observeObject(rt, muts => {
+        muts.forEach(m => {
+            console.log(m)
+            if (m.addedNodes && m.addedNodes.length > 0) {
+                m.target.children.forEach(c => {
+                    console.log(c)
+                    if (c.tagName === 'A' && c.getAttribute('target') === '_blank')
+                        c.removeAttribute('target')
+                })
+            }
+        })
+    })
+}, 3000)
